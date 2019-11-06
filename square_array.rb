53ba12743f40f591f8bd1d7238dcac18dda209bc
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-06 20:44:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,9 @@
 
-def square_array(array)
-  new_array = []
-  array.each{|numbers| new_array.push(numbers**numbers)}
-  new_array
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end
 
-#giht now you have newarray = to the method not its return value .. make newarray and empty array then push the each return to it and call newarray
-
-
-#  array.each{|a| new_array.push(a*a)}
-
-
-# arr = [1, 2, 3, 4, 5]
-# square_array(arr)
