bbefb749fe618a9da160e688994011968314857e
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-06 20:31:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -9,3 +9,5 @@ def square_array(array)
   end 
 new_array
 end
+
+#giht now you have newarray = to the method not its return value .. make newarray and empty array then push the each return to it and call newarray
