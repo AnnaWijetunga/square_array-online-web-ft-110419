d9cb8846be952d29e9ed4854341cb5246aa08a49
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-06 20:32:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -11,3 +11,7 @@ new_array
 end
 
 #giht now you have newarray = to the method not its return value .. make newarray and empty array then push the each return to it and call newarray
+#def square_array(array)
+#  newarray = array.each {|i| i ** 2}
+#  newarray
+# end
